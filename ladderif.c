782e128461d7f58701ad606019a726bce290598b
lioninthewild <prashishd7@gmail.com>
2021-05-31 12:21:37 +0545

    using if-else ladder

---
+++ ladderif.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int a;
+    printf("enter a number: ");
+    scanf("%d", &a);
+    if (a==5){
+        printf("a equals 5");
+    }
+    else if(a<5){
+        printf("a is less than 5");
+    }
+    else{
+        printf("a is greater than 5");
+    }
+    printf("\na= %d",a);
+    return 0;
+}
+
+
+
