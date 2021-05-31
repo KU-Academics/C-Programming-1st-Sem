182fba7e02582afcb651c2db0fbeb205e51e42f0
lioninthewild <prashishd7@gmail.com>
2021-05-31 12:58:45 +0545

    using switch

---
+++ switch.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    char single ;
+    printf("enter a single character: ");
+    scanf("%c",&single);
+    
+    switch(single){
+      case 'x':
+        printf("x\n");
+        break;
+			case 'y' :
+				printf("y\n");
+				break;
+			case 'z' :
+				printf("z\n");
+				break;
+      default :
+				printf("other than x, y and z\n");
+				break;			
+
+    }
+		return 0;
+}
