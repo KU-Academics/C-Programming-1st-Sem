785e4eb6d9f4a5eb9e549da2fa33aa48e4ff9c9a
lioninthewild <prashishd7@gmail.com>
2021-06-09 12:14:55 +0545

    factorial using do while

---
+++ factWhile.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int fact = 1, i = 1, n;
+	printf("enter the number you wish to find the factorial of: ");
+	scanf("%d", &n);
+	if (n >= 0)
+	{
+		do
+		{
+			fact *= i;
+			i += 1;
+		} while (i <= n);
+		printf("the factorial of %d is %d.", n, fact);
+	}
+	else
+	{
+		printf("the factorial of %d is ∞.", n);
+	}
+}
