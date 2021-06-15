f7cf8a30c437f7be449ac5e2cc8bca2db0299410
lioninthewild <prashishd7@gmail.com>
2021-06-15 14:16:23 +0545

    checks for a strong number

---
+++ strong.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+	int x, i, n, num, rem, fact, value = 0;
+	printf("enter the number you wish to check: ");
+	scanf("%d", &num);
+	n = num;
+	for (x = n; x > 0; x /= 10)
+	{
+		rem = n % 10;
+		fact = 1;
+		for (i = 1; i <= rem; ++i)
+		{
+			fact = fact * i;
+		}
+		value += fact;
+		n /= 10;
+	}
+
+	if (value == num)
+	{
+		printf("It is a strong number");
+	}
+	else
+	{
+		printf("It is not a strong number");
+	}
+
+	return 0;
+}
