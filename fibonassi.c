4b8ba7536a894642e6f2d13e6327fed90991af22
lioninthewild <prashishd7@gmail.com>
2021-07-01 14:47:52 +0545

    prints the fibonassi series up to desired terms

---
+++ fibonassi.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+	// printing the fibonassi series
+	int a = 0, b = 1, c = 0;
+	int x, n;
+	//taking input from the user
+	printf("enter number of terms you want to display:");
+	scanf("%d", &n);
+	if (n == 1)
+	{
+		printf("%d\n", a);
+	}
+	else if (n > 0)
+	{
+		printf("%d\n%d\n", a, b);
+		for (x = 0; x < n - 2; x++)
+		{
+			c = (a + b);
+			//printf("%d\n",c);
+			a = b;
+			b = c;
+			printf("%d\n", c);
+		}
+		//printf("%d\n", c);
+	}
+	else
+	{
+		printf("Not a valid no.\n");
+	}
+	return 0;
+}
